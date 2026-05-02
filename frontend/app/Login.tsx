5cfb1649757824b3033f66bbc370fb2955fc563c
Aung Myat <amyat1@sheffield.ac.uk>
2026-05-02 22:40:18 +0100

    Starting to work on the login page

--- frontend/app/Login.tsx
+++ frontend/app/Login.tsx
@@ -1,13 +1,31 @@
-export function Login(){
-
-    
-    return (
-        <div className = "flex items-center justify-center">
-            <div>
-                <form onSubmit={}>
-
-                    </form>
-            </div>
+export function Login() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center w-100 h-100 shadow-2xl rounded-4xl">
+        <div className="flex flex-col">
+          <label htmlFor="uname">
+            <b>Username</b>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            name="uname"
+            required
+          ></input>
         </div>
-    )
+
+        <div>
+          <label htmlFor="psw">
+            <b>Password</b>
+          </label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="psw"
+            required
+          ></input>
+        </div>
+      </div>
+    </div>
+  );
 }
