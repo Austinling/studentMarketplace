d46b07e26d7d69f69c04a5ad946b3d6763928977
Aung Myat <amyat1@sheffield.ac.uk>
2026-05-02 23:20:06 +0100

    Got a good chunk of the login page done

--- frontend/app/Login.tsx
+++ frontend/app/Login.tsx
@@ -1,29 +1,44 @@
+import Image from "next/image";
+
 export function Login() {
   return (
     <div className="flex items-center justify-center">
-      <div className="flex flex-col items-center justify-center w-100 h-100 shadow-2xl rounded-4xl">
-        <div className="flex flex-col">
-          <label htmlFor="uname">
-            <b>Username</b>
-          </label>
+      <div className="flex flex-col items-center justify-center w-96 h-120 shadow-2xl rounded-4xl gap-10">
+        <div>
+          <h3 className="text-4xl font-roboto">Welcome Back!</h3>
+        </div>
+        <div className="flex flex-col gap-6">
           <input
+            className="p-3 border-gray-200 border-2 w-80"
             type="text"
-            placeholder="Enter Username"
+            placeholder="Email Address"
             name="uname"
             required
           ></input>
-        </div>
 
-        <div>
-          <label htmlFor="psw">
-            <b>Password</b>
-          </label>
           <input
+            className="p-3 border-gray-200 border-2 w-80"
             type="password"
-            placeholder="Enter Password"
+            placeholder="Password"
             name="psw"
             required
           ></input>
+        </div>
+        <div className="flex flex-col gap-2">
+          <button className="p-3 w-80 border bg-black text-white">
+            Sign In
+          </button>
+          <button className="p-1 w-80 flex items-center justify-center gap-2 border-gray-200 border-2">
+            <Image
+              className=""
+              src="/google.svg"
+              alt="Google Logo"
+              width={40}
+              height={40}
+              priority
+            />
+            Sign In With Google
+          </button>
         </div>
       </div>
     </div>
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Roboto_Mono } from "next/font/google";
 import "./globals.css";
+
+const robotoMono = Roboto_Mono({
+  weight: "variable",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto-mono",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +32,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${robotoMono.variable} ${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
